91dafa15eb10ab399e7a92e11c902f253369a779
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-04-24 22:13:54 +0200

    matplotlib - adjusted code for reading .png file (using URL) and show image+histogram

--- matplotlib_image1.py
+++ matplotlib_image1.py
@@ -1,13 +1,41 @@
 # import a number of modules and sub-modules for image handling
 import matplotlib.pyplot as plt
+import urllib3
 import matplotlib.image as mpimg
-import numpy as np
+from skimage import io
+from urllib.request import urlopen
 
-# inside of the project directory for this script there is a .png
-# image of a bug for the image handling exercise with matplotlib
-img = mpimg.imread('C:/Users/Luki/Documents/Python Scripts/Anaconda_files/stinkbug.png')
-print(img)
 
-img_plot = plt.imshow(img, cmap="hot")
-plt.colorbar()
-plt.show(img_plot)
+# the .png has been uploaded to github folder with address inside
+# the code = image of a bug for the image handling exercise with
+# matplotlib
+
+# for opening of the image use urlopen from urllib module
+f = urlopen("https://github.com/Teldrin89/Anaconda_files/blob/master/stinkbug.png?raw=true")
+
+# different method url image loading
+# img3 = io.imread("https://github.com/Teldrin89/Anaconda_files/blob/master/stinkbug.png?raw=true")
+
+# use matplotlib pyplot method for reading of the image (assigned
+# to "f" variable from url
+img1 = plt.imread(f)
+
+
+# function for showing image and histogram of said image
+def show_img(img):
+    # display the image
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,3))
+
+    ax1.imshow(img, cmap=plt.cm.gray)
+    ax1.set_axis_off()
+
+    # display the histogram
+    ax2.hist(img.ravel(), lw=0, bins=256)
+    ax2.set_xlim(0, img.max())
+    ax2.set_yticks([])
+    
+    plt.show()
+
+
+# run the function for selected image
+show_img(img1)
