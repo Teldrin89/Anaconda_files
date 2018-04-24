04ef716dda3eb0399e6987cae0b47d8b41487ec5
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-04-24 22:32:27 +0200

    matplotlib - added the image edit - dim and increase contrast and balance

--- matplotlib_image1.py
+++ matplotlib_image1.py
@@ -1,8 +1,6 @@
 # import a number of modules and sub-modules for image handling
 import matplotlib.pyplot as plt
-import urllib3
-import matplotlib.image as mpimg
-from skimage import io
+import skimage.exposure as exp
 from urllib.request import urlopen
 
 
@@ -23,19 +21,27 @@ img1 = plt.imread(f)
 
 # function for showing image and histogram of said image
 def show_img(img):
-    # display the image
+    # display the image - 2 of them for single "fig"  - ax1 (the
+    # image) and ax2 (histogram plot)
+    # (represented by 2 axes) as subplots of single plot window
+    # with set size
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,3))
-
+    # ax1 as image from input variable, grey cmap and no axis
     ax1.imshow(img, cmap=plt.cm.gray)
     ax1.set_axis_off()
 
-    # display the histogram
+    # prepare the histogram
     ax2.hist(img.ravel(), lw=0, bins=256)
     ax2.set_xlim(0, img.max())
     ax2.set_yticks([])
-    
+    # display plot
     plt.show()
 
 
 # run the function for selected image
 show_img(img1)
+# a way to dim the image
+# show_img(exp.rescale_intensity(img1,
+# in_range=(0.4, .95), out_range=(0, 1)))
+# a method to increase contrast and balance of the picture
+show_img(exp.equalize_adapthist(img1))
